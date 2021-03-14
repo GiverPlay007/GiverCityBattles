bf80f679c6a76a4e54432edd6e760df1a723f55c
GiverPlay007 <play.giver.007@gmail.com>
2021-03-13 22:19:40 -0300

    FPS limit

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,27 @@ Game *game = nullptr;
 
 int main(int args, char *argv[])
 {
+	const int FPS = 60;
+	const int frameDelay = FPS / 60;
+
+	Uint32 frameStart;
+	int frameTime;
+
 	game = new Game();
 	game->init("Givercity Battles", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 460, 680, false);
 
 	while (game->isRunning()) {
+		frameStart = SDL_GetTicks();
+
 		game->handleEvents();
 		game->update();
 		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime) {
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->clean();
